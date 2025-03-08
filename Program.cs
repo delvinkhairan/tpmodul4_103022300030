78d9aad6ce25e2069ed8c9217451d754bc801a4c
delvinkhairan <delvinkhairan9558@gmail.com>
2025-03-09 00:08:19 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
